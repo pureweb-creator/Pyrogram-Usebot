03a9f940a0003f01f8c832957c29293ba4261f0b
Roman <roman.semenikhin99@gmail.com>
2022-02-05 01:55:41 +0200

    upd

--- main.py
+++ main.py
@@ -58,9 +58,6 @@ cats = cats.split(',')
 @app.on_message(filters.user(cfg.allowed_users))
 async def main(client, message):
     try:
-        print(client)
-        print(message)
-
         if(message.animation):   
             file_name = str(uuid.uuid4().hex)
             await client.download_media(message,"./downloads/"+file_name+".mp4")
